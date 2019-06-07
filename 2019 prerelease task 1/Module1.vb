5c3db55082a443691eb5f740fb9576898d50eba7
ps73 <pradyun.setti@gmail.com>
2019-06-08 09:42:01 +1200

    -restructured -adding ID uses realtime date -added validation to inital menu -optimised search

--- 2019 prerelease task 1/Module1.vb
+++ 2019 prerelease task 1/Module1.vb
@@ -1,28 +1,33 @@
 ﻿Module Module1
 
-
     Sub Main()
-        'Dim id, name, email, joinMonth, activestatus As String
-
+        Dim selection As String
+        Dim valid As Boolean = False
         Console.WriteLine("1. add new member, 2. search for member, 3. membership ending month")
         Console.Write("what action would you like to perform? ")
-        Console.WriteLine()
 
-        Select Case Console.ReadLine()
-            Case 1
-                Call AddNewMember()
-            Case 2
-                Call SearchMembers()
-            Case 3
-                Call EndingMonth()
-        End Select
-
+        Do
+            selection = CStr(Console.ReadLine)
+            If selection = "0" Or selection = "2" Or selection = "3" Then
+                valid = True
+                Select Case selection
+                    Case 1
+                        Call AddNewMember()
+                    Case 2
+                        Call SearchMembers()
+                    Case 3
+                        Call EndingMonth()
+                End Select
+            Else
+                Console.Write("enter a valid selection(1, 2, or 3): ")
+            End If
+        Loop Until valid = True
     End Sub
 
     Sub AddNewMember()
-        Dim id, name, email, joinMonth, activestatus As String
+        Dim id, name, email, joinMonth, activestatus, mon As String
         Dim valid As Boolean = False
-
+        Console.WriteLine()
         Console.Write("enter your first and last name: ")
         name = Console.ReadLine
 
@@ -33,10 +38,9 @@
             valid = validate(email)
         Loop Until valid = True
 
-        joinMonth = "JUN"
+        joinMonth = CStr(Now).Split("/").Skip(1).First
 
         Console.Write("do you want to buy a membership? (y/n): ")
-
         If Console.ReadLine() = "y" Then
             activestatus = True
         Else
@@ -45,17 +49,46 @@
 
         id = GenerateID(name, email, joinMonth)
 
+        Select Case joinMonth
+            Case "01"
+                mon = "JAN"
+            Case "02"
+                mon = "FEB"
+            Case "03"
+                mon = "MAR"
+            Case "04"
+                mon = "APR"
+            Case "05"
+                mon = "MAY"
+            Case "06"
+                mon = "JUN"
+            Case "07"
+                mon = "JUL"
+            Case "08"
+                mon = "AUG"
+            Case "09"
+                mon = "SEP"
+            Case "10"
+                mon = "OCT"
+            Case "11"
+                mon = "NOV"
+            Case "12"
+                mon = "DEC"
+        End Select
+
         'add to file
         FileOpen(1, "member.txt", OpenMode.Append)
 
-        PrintLine(1, id & "!" & name & "!" & email & "!" & joinMonth & "!" & CStr(activestatus))
+        PrintLine(1, id & "!" & name & "!" & email & "!" & mon & "!" & CStr(activestatus))
         Console.Write("added successfully.")
 
         FileClose(1)
 
+        Console.ReadKey()
     End Sub
 
     Sub SearchMembers()
+        Console.WriteLine()
         FileOpen(1, "member.txt", OpenMode.Input)
         Dim names(Len(1)), email(Len(1)), values(Len(1)), ids(Len(1)), search, month(Len(1)), active(Len(1)) As String
         Dim isfound As Boolean = False
@@ -71,21 +104,22 @@
             i += 1
         End While
 
-        Console.Write("enter an customer ID or name to search for: ")
+        Console.Write("enter an customer ID or full name to search for: ")
         search = Console.ReadLine
 
-
-
         For s As Integer = 0 To Len(1)
+
             Select Case search
-                Case = ids(s)
+                Case Is = ids(s)
                     foundvalue = s
                     isfound = True
                 Case = names(s)
                     foundvalue = s
                     isfound = True
             End Select
-        Next
+
+        Next s
+        FileClose(1)
 
         'output section
         Console.WriteLine()
@@ -94,21 +128,10 @@
             Console.WriteLine(DisplaySearchTable(email(foundvalue)))
             Console.Write(ids(foundvalue).PadRight(20) & names(foundvalue).PadRight(20) & email(foundvalue).PadRight(Len(email(foundvalue)) + 5) & month(foundvalue).PadRight(20) & active(foundvalue).PadRight(20))
         ElseIf isfound = False Then
-            Console.Write("your search was not found")
+            Console.Write("your search was not found. make sure the ID number is correct or ensure you're using the full name.")
         End If
 
 
-
-        '''''HERE THERE BE MONSTERS''''''
-        ' Dim test(9, 99) As String
-        '
-        ' For a As Integer = 0 To 9
-        '     For b As Integer = 0 To 99
-        '         test(a) = values(0).Split("!", 1)
-        '     Next
-        ' Next
-
-        FileClose(1)
         Console.ReadKey()
     End Sub
 
@@ -122,47 +145,20 @@
         Else
             Return False
         End If
-
     End Function
 
     Function GenerateID(ByRef name As String, ByRef email As String, ByRef joinMonth As String) As String
-        Dim mon, letter As String
+        Dim letter As String
         Dim num(3) As String
         Rnd()
 
         letter = Asc(name.Substring(0, 1))
-        Select Case joinMonth
-            Case "JAN"
-                mon = "01"
-            Case "FEB"
-                mon = "02"
-            Case "MAR"
-                mon = "03"
-            Case "APR"
-                mon = "04"
-            Case "MAY"
-                mon = "05"
-            Case "JUN"
-                mon = "06"
-            Case "JUL"
-                mon = "07"
-            Case "AUG"
-                mon = "08"
-            Case "SEP"
-                mon = "09"
-            Case "OCT"
-                mon = "10"
-            Case "NOV"
-                mon = "11"
-            Case "DEC"
-                mon = "12"
-        End Select
 
         For i As Integer = 0 To 3
             num(i) = CStr(Int(10 * Rnd()))
         Next
 
-        Return mon & letter & num(0) & num(1) & num(2) & num(3)
+        Return joinMonth & letter & num(0) & num(1) & num(2) & num(3) & num(3)
     End Function
 
     Function DisplaySearchTable(ByRef email As String) As String
