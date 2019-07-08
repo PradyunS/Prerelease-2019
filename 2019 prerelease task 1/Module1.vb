081b543c53cf997b2d593baabdcaee573f05d0a1
ps73 <pradyun.setti@gmail.com>
2019-07-08 22:03:46 +1200

    really minor tweak: clear console when going home

--- 2019 prerelease task 1/Module1.vb
+++ 2019 prerelease task 1/Module1.vb
@@ -285,6 +285,7 @@
         Console.WriteLine()
         Console.Write("enter h to go back home or enter key to exit: ")
         If Console.ReadLine = "h" Then
+            Console.Clear()
             Call Main()
         End If
     End Sub
